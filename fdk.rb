8be1b57db442198733b84847de7db04f7cbd2be9
Nadeem Bhati <nadeem.bhati@freshworks.com>
2024-02-07 12:29:12 +0530

    fdk brew

---
+++ fdk.rb
@@ -0,0 +1,46 @@
+require "language/node"
+
+class Fdk < Formula
+  desc "Freshworks Developer Kit"
+  license "MIT"
+  homepage "https://developer.freshworks.com/"
+ 
+  url "https://nodejs.org/dist/v18.18.2/node-v18.18.2-darwin-x64.tar.gz"
+  sha256 "5bb8da908ed590e256a69bf2862238c8a67bc4600119f2f7721ca18a7c810c0f"
+
+  
+  # Define the resource
+  resource "node" do
+    url "https://nodejs.org/dist/v18.18.2/node-v18.18.2-darwin-x64.tar.gz"
+    sha256 "5bb8da908ed590e256a69bf2862238c8a67bc4600119f2f7721ca18a7c810c0f" 
+  end
+
+
+  def install
+    target_dir = prefix/"custom_node_location" 
+
+    resource("node").stage do
+      target_dir.install Dir["*"]
+      ohai "Node.js installed to #{target_dir}"
+    end
+
+    ENV.prepend_path "PATH", "#{prefix}/custom_node_location/bin"
+    node_version_output = Utils.safe_popen_read("node", "--version").strip
+   
+    ohai "Node.js version: #{node_version_output}"
+
+    node_version_output = Utils.safe_popen_read("npm", "--version").strip
+  
+   
+    ohai "npm.js version: #{node_version_output}"
+
+    # npm config set prefix /usr/local/Cellar/fdk/64/custom_node_location 
+    system "#{prefix}/custom_node_location/bin/npm", "config", "set", "prefix", "#{prefix}/custom_node_location"
+    system "#{prefix}/custom_node_location/bin/npm", "install", "-g", "https://cdn.freshdev.io/fdk/latest.tgz"
+
+  end
+
+  test do
+    system true
+  end
+end
