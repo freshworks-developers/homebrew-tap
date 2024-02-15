57ad52d553e4504b75f5b3c9b0c92681fed06321
Nadeem Bhati <nadeem.bhati@freshworks.com>
2024-02-15 09:42:46 +0530

    Update fdk.rb

--- fdk.rb
+++ fdk.rb
@@ -4,7 +4,6 @@ class Fdk < Formula
   desc "Freshworks Developer Kit"
   license "MIT"
   homepage "https://developer.freshworks.com/"
- 
   url "https://nodejs.org/dist/v18.18.2/node-v18.18.2-darwin-x64.tar.gz"
   sha256 "5bb8da908ed590e256a69bf2862238c8a67bc4600119f2f7721ca18a7c810c0f"
 
@@ -12,7 +11,7 @@ class Fdk < Formula
   # Define the resource
   resource "node" do
     url "https://nodejs.org/dist/v18.18.2/node-v18.18.2-darwin-x64.tar.gz"
-    sha256 "5bb8da908ed590e256a69bf2862238c8a67bc4600119f2f7721ca18a7c810c0f" 
+    sha256 "5bb8da908ed590e256a69bf2862238c8a67bc4600119f2f7721ca18a7c810c0f"
   end
 
 
@@ -24,18 +23,20 @@ class Fdk < Formula
       ohai "Node.js installed to #{target_dir}"
     end
 
+    # Add the node and npm to the PATH
     ENV.prepend_path "PATH", "#{prefix}/custom_node_location/bin"
+
+    # Verify the installation
     node_version_output = Utils.safe_popen_read("node", "--version").strip
-   
     ohai "Node.js version: #{node_version_output}"
 
-    node_version_output = Utils.safe_popen_read("npm", "--version").strip
-  
-   
-    ohai "npm.js version: #{node_version_output}"
+    npm_version_output = Utils.safe_popen_read("npm", "--version").strip
+    ohai "npm.js version: #{npm_version_output}"
 
     # npm config set prefix /usr/local/Cellar/fdk/64/custom_node_location 
     system "#{prefix}/custom_node_location/bin/npm", "config", "set", "prefix", "#{prefix}/custom_node_location"
+
+    # install latest FDK
     system "#{prefix}/custom_node_location/bin/npm", "install", "-g", "https://cdn.freshdev.io/fdk/latest.tgz"
 
   end
